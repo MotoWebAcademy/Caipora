36ceb49b9f542f49315ac7b7d1237bc0ddaf2f55
MotoWebAcademy <157655943+MotoWebAcademy@users.noreply.github.com>
2024-02-26 19:36:39 -0300

    Update index.ts(antonimos)

--- API/index.ts
+++ API/index.ts
@@ -4,40 +4,57 @@ import cheerio from 'cheerio';
 import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 3000; //define porta dp server
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.get('/search/:word', async (req: Request, res: Response) => { //definindo a rota
+app.get('/search/:word', async (req: Request, res: Response) => {
   const word = req.params.word;
   try {
-    const result = await scrapeDicio(word); //chama a função que vai pegar os significados
-    res.json(result); //retorna um json
+    const result = await scrapeDicio(word);
+    res.json(result);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao obter informações da palavra.' });
   }
 });
 
-app.listen(port, () => { //inicia o servidpr
+app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
-//funcao que faz o web scraping no site dicio
-async function scrapeDicio(word: string): Promise<{ word: string; meaning: string; additionalInfo: string; phrases: string }> {
+async function scrapeDicio(word: string): Promise<{ word: string; meaning: string; additionalInfo: string; phrases: string; antonyms?: string[] }> {
   const url = `https://www.dicio.com.br/${word}`;
   const response = await axios.get(url);
 
   if (response.status === 200) {
     const $ = cheerio.load(response.data);
 
-    // Puxar o conteúdo relevante da palavra
     const meaning = $('p.significado.textonovo').first().text().trim();
     const additionalInfo = $('p.adicional:not(.sinonimo)').text().trim();
     const phrases = $('div.frase').text().trim();
 
-    return { word, meaning, additionalInfo, phrases };
+    // Chama a função para obter os antônimos
+    const antonyms = await scrapeAntonimos(word);
+
+    return { word, meaning, additionalInfo, phrases, antonyms };
   } else {
     throw new Error('Erro ao obter a página.');
+  }
+}
+
+async function scrapeAntonimos(word: string): Promise<string[]> {
+  const antonimosUrl = `https://www.antonimos.com.br/${word}`;
+  const antonimosResponse = await axios.get(antonimosUrl);
+
+  if (antonimosResponse.status === 200) {
+    const $ = cheerio.load(antonimosResponse.data);
+
+    // Puxa os antônimos do site
+    const antonyms = $('p.ant-list').first().text().trim().split(', ');
+
+    return antonyms;
+  } else {
+    throw new Error('Erro ao obter os antônimos.');
   }
 }
