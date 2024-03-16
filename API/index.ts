8ef39c5c34651b2e7ec09a576af9b543d2f8120e
MotoWebAcademy <157655943+MotoWebAcademy@users.noreply.github.com>
2024-03-15 22:30:26 -0400

    Update index.ts - palavra do dia, pesquisa de palavras com ç, correção das 10 mais pesquisadas

--- API/index.ts
+++ API/index.ts
@@ -26,10 +26,11 @@ app.use(cors());
 // Rota para realizar a busca da palavra
 app.get('/search/:word', async (req: Request, res: Response) => {
   const word = req.params.word;
+  const encodedWord = encodeURIComponent(word);  // Codifica a palavra para a URL
   try {
     const result = await scrapeDicio(word);
 
-    await saveRecentSearch(word);
+    await saveRecentSearch(encodedWord);
 
     res.json(result);
   } catch (error) {
@@ -66,21 +67,87 @@ async function saveRecentSearch(word: string) {
   });
 }
 
-// Função para obter as últimas pesquisas do Firebase
+// Função para obter as últimas pesquisas do Firebase sem repetições
 async function getRecentSearches(): Promise<string[]> {
   const db = admin.firestore();
   const searchesRef = db.collection('searches');
 
-  // Ordene as pesquisas por timestamp em ordem decrescente e limite a 10
+  // Obtenha as últimas pesquisas do Firebase
   const snapshot = await searchesRef.orderBy('timestamp', 'desc').limit(10).get();
-  const searches = snapshot.docs.map((doc) => doc.data().word);
+  const uniqueSearches = new Set<string>();
+
+  snapshot.docs.forEach((doc) => {
+    const word = doc.data().word;
+    uniqueSearches.add(word);
+  });
+
+  // Converta o conjunto para um array
+  const searches = Array.from(uniqueSearches);
 
   return searches;
 }
 
 // Função para realizar o web scraping no site dicio
-async function scrapeDicio(word: string): Promise<{ word: string; meaning: string; additionalInfo: string; phrases: string; antonyms?: string[] }> {
-  const url = `https://www.dicio.com.br/${word}`;
+async function scrapeDicio(word: string): Promise<{ word: string; meaning?: string; additionalInfo?: string; phrases?: string; antonyms?: string[]; hrefContent?: string  }> {
+  const encodedWord = encodeURIComponent(word);  // Codifica a palavra para a URL
+  let url: string;
+
+  // Verifica se a palavra tem "c" ou "ç" no meio
+  if (/c|ç/.test(word.slice(1, -1))) {
+    url = `https://www.dicio.com.br/pesquisa.php?q=${encodedWord}`;
+  } else {
+    url = `https://www.dicio.com.br/${encodedWord}`;
+  }
+
+  const response = await axios.get(url);
+
+  if (response.status === 200) {
+    const $ = cheerio.load(response.data);
+
+    let hrefContent: string | undefined;
+    let meaning: string | undefined;
+    let additionalInfo: string | undefined;
+    let phrases: string | undefined;
+    let antonyms: string[] | undefined;
+
+    // Verifica se a palavra tem "c" ou "ç" no meio
+    if (/c|ç/.test(word.slice(1, -1))) {
+      // Obtém o conteúdo do href
+      hrefContent = $('a._sugg').first().attr('href') || undefined;
+
+      // Incrementa a URL base
+      const fullUrl = `https://www.dicio.com.br${hrefContent}`;
+      
+      // Realiza scraping na URL completa para obter as informações padrão
+      const scrapeResult = await scrapeStandardInfo(fullUrl, word);
+      
+      // Atualiza os campos com base nos resultados do scraping
+      meaning = scrapeResult.meaning;
+      additionalInfo = scrapeResult.additionalInfo;
+      phrases = scrapeResult.phrases;
+      antonyms = scrapeResult.antonyms;
+    } else {
+      // Obtém o significado da palavra
+      meaning = $('p.significado.textonovo').first().text().trim();
+
+      // Obtém informações adicionais, frases e antônimos
+      additionalInfo = $('p.adicional:not(.sinonimo)').text().trim();
+      phrases = $('div.frase').text().trim();
+      try {
+        antonyms = await scrapeAntonimos(word);
+      } catch (error) {
+        console.error((error as Error).message);
+      }
+    }
+
+    return { word, meaning, additionalInfo, phrases, antonyms, hrefContent  };
+  } else {
+    throw new Error('Erro ao obter a página.');
+  }
+}
+
+// Função para realizar scraping na URL completa para obter as informações padrão
+async function scrapeStandardInfo(url: string, word: string): Promise<{ meaning?: string; additionalInfo?: string; phrases?: string; antonyms?: string[] }> {
   const response = await axios.get(url);
 
   if (response.status === 200) {
@@ -89,8 +156,6 @@ async function scrapeDicio(word: string): Promise<{ word: string; meaning: strin
     const meaning = $('p.significado.textonovo').first().text().trim();
     const additionalInfo = $('p.adicional:not(.sinonimo)').text().trim();
     const phrases = $('div.frase').text().trim();
-
-    // Chama a função para obter os antônimos
     let antonyms: string[] | undefined;
     try {
       antonyms = await scrapeAntonimos(word);
@@ -98,7 +163,7 @@ async function scrapeDicio(word: string): Promise<{ word: string; meaning: strin
       console.error((error as Error).message);
     }
 
-    return { word, meaning, additionalInfo, phrases, antonyms };
+    return { meaning, additionalInfo, phrases, antonyms };
   } else {
     throw new Error('Erro ao obter a página.');
   }
@@ -120,3 +185,28 @@ async function scrapeAntonimos(word: string): Promise<string[]> {
     throw new Error('Erro ao obter os antônimos.');
   }
 }
+
+// Função para realizar o scraping do conteúdo da tag div#dia
+async function scrapeWordOfTheDay(): Promise<string> {
+  const url = 'https://www.dicio.com.br/';
+  const response = await axios.get(url);
+
+  if (response.status === 200) {
+    const $ = cheerio.load(response.data);
+    const wordOfTheDay = $('div.word-of-day--widget').text().trim();
+    return wordOfTheDay;
+  } else {
+    throw new Error('Erro ao obter a página do Dicio.');
+  }
+}
+
+// Rota para obter a palavra do dia
+app.get('/dia', async (req: Request, res: Response) => {
+  try {
+    const wordOfTheDay = await scrapeWordOfTheDay();
+    res.send(wordOfTheDay);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao obter a palavra do dia.');
+  }
+});
